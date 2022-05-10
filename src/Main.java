45a02e1d7e6caf3352a394606a0af45516d1c069
Kseniya <barkas074@yandex.ru>
2022-05-10 23:09:02 +0500

    completed task02

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicInteger;
+
 public class Main {
     public static void main(String[] args) throws InterruptedException {
-        Thread t1 = new Lucky.LuckyThread();
+        /*Thread t1 = new Lucky.LuckyThread();
         Thread t2 = new Lucky.LuckyThread();
         Thread t3 = new Lucky.LuckyThread();
         t1.start();
@@ -9,6 +14,42 @@ public class Main {
         t2.join();
         t3.start();
         t3.join();
-        System.out.println("Total: " + Lucky.count);
+        System.out.println("Total: " + Lucky.count);*/
+
+        /*// создаем пул для выполнения наших задач
+        //   максимальное количество созданных задач - 3
+        ThreadPoolExecutor executor = new ThreadPoolExecutor(
+                // не изменяйте эти параметры
+                3, 3, 1, TimeUnit.SECONDS, new LinkedBlockingQueue<>(3));
+
+        // сколько задач выполнилось
+        AtomicInteger count = new AtomicInteger(0);
+
+        // сколько задач выполняется
+        AtomicInteger inProgress = new AtomicInteger(0);
+
+        // отправляем задачи на выполнение
+        for (int i = 0; i < 30; i++) {
+            final int number = i;
+            Thread.sleep(10);
+
+            while (executor.getActiveCount() == executor.getMaximumPoolSize())
+                Thread.sleep(10);
+
+            System.out.println("creating #" + number);
+            executor.submit(() -> {
+                int working = inProgress.incrementAndGet();
+                System.out.println("start #" + number + ", in progress: " + working);
+                try {
+                    // тут какая-то полезная работа
+                    Thread.sleep(Math.round(1000 + Math.random() * 2000));
+                } catch (InterruptedException e) {
+                    // ignore
+                }
+                working = inProgress.decrementAndGet();
+                System.out.println("end #" + number + ", in progress: " + working + ", done tasks: " + count.incrementAndGet());
+                return null;
+            });
+        }*/
     }
 }
